67c2ed5e21a443c09b4da769555f231db833514f
Ekaluk52003 <ekaluk.p@gmail.com>
2025-07-28 11:16:28 +0700

    color bg nav

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <!-- Language Switcher -->
-            <div class="bg-indigo-950 dark:bg-gray-800">
+            <div class="bg-slate-800 dark:bg-gray-800">
                 <div class="flex justify-between items-center px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="flex items-center space-x-2 text-white">
                         <a href="{{ request()->fullUrlWithQuery(['locale' => 'en']) }}" class="text-sm hover:text-gray-200 {{ app()->getLocale() === 'en' ? 'font-bold underline' : '' }}">English</a>

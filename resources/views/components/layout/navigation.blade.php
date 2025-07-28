febd33063e2b0b75cda461599595c121950da24a
Ekaluk52003 <ekaluk.p@gmail.com>
2025-07-28 11:39:50 +0700

    cat menu

--- resources/views/components/layout/navigation.blade.php
+++ resources/views/components/layout/navigation.blade.php
@@ -20,27 +20,32 @@
                         {{ __('frontend.fruits') }}
                     </x-ui.nav-link>
                     
-                    <x-ui.dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="inline-flex items-center h-16 px-3 py-18 text-sm font-medium text-white transition duration-150 ease-in-out border-transparent hover:text-yellow-400 focus:outline-none {{ request()->routeIs('fruits.*') && request()->has('category_id') ? 'text-yellow-400' : '' }}">
-                                <div>{{ __('frontend.category') }}</div>
+                    <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false" class="relative">
+                        <button @click="open = !open" class="inline-flex items-center px-1 pt-2 h-16 border-b-2 border-transparent text-sm font-medium leading-5 text-white hover:text-yellow-400 hover:border-yellow-300 focus:outline-none focus:text-yellow-400 focus:border-yellow-300 transition duration-150 ease-in-out {{ request()->routeIs('fruits.*') && request()->has('category_id') ? 'border-yellow-400 text-yellow-400' : '' }}">
+                            {{ __('frontend.category') }}
+                            <svg class="ml-1 w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
 
-                                <div class="ms-1">
-                                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            @foreach($navCategories as $category)
-                                <x-ui.dropdown-link :href="route('fruits.index') . '?locale=' . app()->getLocale() . '&category_id=' . $category->id . '&sort=name&direction=asc'">
-                                    {{ $category->name }}
-                                </x-ui.dropdown-link>
-                            @endforeach
-                        </x-slot>
-                    </x-ui.dropdown>                    
+                        <div x-show="open"
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="transform opacity-0 scale-95"
+                            x-transition:enter-end="transform opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-75"
+                            x-transition:leave-start="transform opacity-100 scale-100"
+                            x-transition:leave-end="transform opacity-0 scale-95"
+                            class="absolute right-0 z-50 mt-2 w-48 rounded-md shadow-lg origin-top-right"
+                            style="display: none;">
+                            <div class="py-1 bg-white rounded-md ring-1 ring-black ring-opacity-5">
+                                @foreach($navCategories as $category)
+                                    <a href="{{ route('fruits.index') }}?locale={{ app()->getLocale() }}&category_id={{ $category->id }}&sort=name&direction=asc" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+                                        {{ $category->name }}
+                                    </a>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>                    
                     
                     <x-ui.nav-link :href="route('quote.index') . '?locale=' . app()->getLocale()" :active="request()->routeIs('quote.*')">
                         {{ __('frontend.request_quote_menu') }}

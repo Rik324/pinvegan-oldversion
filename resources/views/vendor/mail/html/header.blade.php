d49c44f06b092d46f5e2fc9fe2ca64e4ccd05f9b
Rik324 <rishabhkool786@gmail.com>
2025-08-02 23:23:15 +0700

    new logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/branding/logo.jpg') }}" class="logo" alt="{{ config('app.name') }} Logo">
+<img src="{{ asset('images/branding/pinveganlogo.png') }}" class="logo" alt="{{ config('app.name') }} Logo">
 </a>
 </td>
 </tr>
